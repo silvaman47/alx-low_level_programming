44ef1ede1114faad2de1f86997a33dc55a00c6ef
Silas Awese Aosige <aosige47@gmail.com>
2023-02-16 12:14:20 +0000

    choiceless

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
-#!/bin/bash
 #include <stdio.h>
 
-
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-	int in;
-    float fl;
-    char ch;
-    long lo;
-    long long ll;
+	char c;
+	int i;
+	long li;
+	long long lii;
+	float f;
 
-	printf("Size of a char: %ld byte(s)\n", sizeof(ch));
-	printf("Size of an int: %ld byte(s)\n", sizeof(in));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(lo));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
-	printf("Size of a float: %ld byte(s)\n", sizeof(fl));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lii));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
