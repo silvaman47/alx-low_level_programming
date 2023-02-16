6d02e48c5a4489aff10abd4ffa4b8d177ab64abd
Silas Awese Aosige <aosige47@gmail.com>
2023-02-16 12:09:29 +0000

    choiceless

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #!/bin/bash
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
