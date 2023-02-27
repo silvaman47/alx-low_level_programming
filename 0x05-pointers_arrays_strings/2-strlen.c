73901d1e7f134d60d7c553fa5f1bd970eda8d0d7
Silas Aosige <aosige47@gmail.com>
2023-02-27 16:51:45 +0000

    27th

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,145 @@
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: string parameter input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; ++s)
+		++counter;
+
+	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}/**
+    * _strlen - returns the length of a string
+    *  *
+    *   * @s: string parameter input
+    *    *
+    *     * Return: length of string
+    *     */
+
+int _strlen(char *s)
+{
+  	int counter;
+
+    	for (counter = 0; *s != '\0'; ++s)
+          		++counter;
+
+        	return (counter);
+}
